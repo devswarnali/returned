b575a4cd9969b720ef456f9e703018cdec521773
devswarnali <166713710+devswarnali@users.noreply.github.com>
2025-03-23 23:04:22 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,7 +11,46 @@ if ($(".effortless-returns-slide").length) {
       direction: 'ltr',
       autoWidth: true,
       autoScroll: {
-        speed: 1.2,
+        speed: 1.5,
       },
     }).mount(window.splide.Extensions);
 }
+
+$('.navbar-toggler').on('click', function(){
+  $('body').toggleClass('o-hidden');
+})
+
+// how-it-work-mobile-slider
+
+// $('.slider').slick({
+//   dots: true,
+//   arrow: false,
+//   fade: true,
+//   speed: 1800,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+// })
+
+$(function() {
+  $('.work-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      mobileFirst: true,
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      responsive: [
+          {
+              breakpoint: 767,
+              settings: 'unslick'
+          }
+      ]
+  });
+
+  $(window).on('resize', function() {
+      $('.work-slider').slick('resize');
+  });
+});
